bd014ce6b58d720a2953a1596db7035a5ae0e8c8
Matthew <matthew.salvino@duke.edu>
2019-09-09 12:28:54 -0400

    Design cholesterol interface

--- calculator.py
+++ calculator.py
@@ -1,3 +1,18 @@
+def check_HDL(HDL_result):
+	if HDL_result >= 60:
+		return "Normal"
+	elif 40 <= HDL < 60:
+		return "Boderline low"
+	else:
+		return "Low"
+
+def cholesterol_interface():
+	print("Cholesterol check")
+	chol_input = input("Enter your cholesterol test result: ")
+	chol_data = chol_input.split("=")
+	if chol_data[0] == "HDL"
+		result = check_HDL(chol_data[1])
+
 def interface():
 	print("My calculator program")
 	keep_running = True
